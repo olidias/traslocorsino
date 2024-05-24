1ea0adbc11f4683ef4cbd139327fbefae4cbbee2
Oli Dias <info@dscs.io>
2024-05-24 14:59:07 +0200

    various fixes, seo, lighthouse

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,25 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://traslocorsino.ch',
   i18n: {
     defaultLocale: "de",
     locales: ["de", "en"],
     routing: {
-      prefixDefaultLocale: false,
-  }
+      prefixDefaultLocale: false
+    }
   },
-  integrations: [tailwind(), react(),icon()],
+  integrations: [tailwind(), react(), icon(), sitemap({
+    i18n: {
+      defaultLocale: 'de',
+      locales:  {
+        de: 'de-CH',
+        en: 'en'
+      }
+    }
+  })]
 });
